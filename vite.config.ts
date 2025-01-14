bca5890af9e1807093ecee44877ebee6db90e50a
Shivam Raghav <shivamraghav00000@gmail.com>
2025-01-15 02:54:06 +0530

    outDir as dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   server: {
     port: 5175
   },
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // Change to 'dist'
     chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules')) {
-            return 'vendor'; // Group node_modules into a vendor chunk
+            return 'vendor';
           }
         }
       }
     }
   },
   optimizeDeps: {
-    include: ['react', 'react-dom'], // Pre-bundle essential dependencies
+    include: ['react', 'react-dom']
   }
 });
